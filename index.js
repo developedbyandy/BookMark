7f311e48cfac84264592f146fb5ed0d0a60cc6da
Andy Charles <66924525+developedbyandy@users.noreply.github.com>
2020-08-27 20:25:44 -0400

    Changes made to loadbookmark

--- index.js
+++ index.js
@@ -1,79 +1,96 @@
-import $ from "jquery";
-import {STORE} from "./store.js";
-import {generateForm, generateTextField, generateButton} from "./generateForm.js";
+import $ from 'jquery';
+import bookmarks from './store';
+import cuid from 'cuid';
+const urlEndpoint = "https://thinkful-list-api.herokuapp.com/andycharles/bookmarks";
+//Add bookmarks to my bookmark list
+//I can see a list of my bookmarks
+//I can click on a bookmark to display the detailed view
+//I can remove bookmarks from my bookmark list
+// I receive appropriate server feedback when I cannot add/update a bookmark
+//I can select froma dropdown a "minimum rating" to filter the list by all bookmarks
+//rated equal or above the chosen selection
 
-export function init() 
-{
-    //DisplayHello();
-    renderHeader();
-    renderForm();
-    loadBookmarkList();
-}
-/*export function DisplayHello() {
-    console.log('hello');
-    console.log(STORE);
-    console.log(generateForm());
-}*/
-const rootStyle = 
-{
-    "display": "grid",
-    "grid-template-rows": "100px 700px",
-    "grid-gap": "5px",
-    "width": "100%",
-    "height": "100%"
+export function init(){
+
+  loadBookMarks();
 }
 
-let renderHeader = () => {
-    $("#root").append(`
-    <header>Bookmark Application</header>
-    `)
-}
 
-let renderForm = () =>
-{
-    $("#root").append(`<div class="formComponent"><form action="" method="get" id="addBookmarkForm">
-    ${generateTextField("Enter title ", "bookmarkTitle")}
-    <br>
-    ${generateTextField("Enter URL ", "urlDescription")}
-    <br>
-    ${generateButton("Add Bookmark", "addBookmark")}
-    </form>
-    <div id="bookmarkList">
-    <ul></ul></div>
-    </div>
-    `);
-    //Form event 
-    $("#addBookmarkForm").submit(submitEvent)
-    //Style form
-    $("#root").css(rootStyle);
-    //$(".formComponent").css(formStyle);
-}
 
-let submitEvent = (evt)=>{
-    evt.preventDefault();
-    console.log("Form clicked!");
-    let fields = $( ":input" ).serializeArray();
-    const object = {
-        title: fields[0]["value"],
-        url: fields[1]["value"],
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi auctor felis ac ante tincidunt, nec mattis ante vestibulum. Morbi et arcu eu odio tincidunt feugiat nec sit amet libero. Proin justo nibh, dignissim eu tincidunt porttitor, placerat nec magna. Sed dui leo, sodales a pellentesque a, fermentum tempor mauris. Etiam ac posuere velit, ac euismod risus. Sed libero ante, eleifend laoreet enim eu, consequat egestas elit. Aliquam eget lacus eget ante sollicitudin bibendum sit amet vitae sapien. Etiam congue interdum blandit. Vestibulum ut nulla nisl. Nulla tristique at libero et rutrum. Aliquam in tortor in quam cursus aliquam. Donec ultrices lectus ac molestie dictum. Ut sodales metus in risus varius fermentum."
+function main() {
+  console.log('DOM is loaded');
+  console.log(bookmarks);
+  const startMsg = $('<p>Webpack is working!</p>');
+  $('#root').append(startMsg);
+  $("#js-bookmark-form").submit(function (event) {
+    event.preventDefault();
+    let bookmarkEntry = $("#bookmark-entry").val();
+    let bookmarkUrl = $('#bookmark-url').val();
+    let newBookmark = {
+      'id': cuid(),
+      'title': bookmarkEntry,
+      'url': bookmarkUrl
+
     }
-    STORE.push(object);
-    clearBookmarkList();
-    loadBookmarkList();
+    addBookmark(newBookmark)
+    clearBookmarkList()
+    setTimeout(function(){ loadBookMarks(); }, 1000);
+    console.log('test');
+  });
+  loadBookMarks();
 }
 
-let loadBookmarkList = () => {
-    STORE.forEach((bookmark)=> {
-        $("#bookmarkList ul").append(
-            `<li><h2>${bookmark.title}<h2></li>
-            <li><i>${bookmark.url}</i></li>
-            <li><p>${bookmark.description}</p></li>`
-        )
-    })
+let loadBookMarks = function () {
+  fetch(urlEndpoint, { method: 'GET' })
+    .then(res => res.json())
+    .then(json => {
+      const bookmarks = json;
+      console.log(Array.isArray(bookmarks));
+     bookmarks.forEach((bookmark) => {
+        $('#bookmarkList').append('<ul>')
+        $('#bookmarkList').append(`<li class= "title">${bookmark.title}</li>`)
+        $('#bookmarkList').append(`<li>${bookmark.description}</li>`)
+        $('#bookmarkList').append(`<li>${bookmark.url}</li>`)
+        $('#bookmarkList').append(`<li>${bookmark.rating}</li>`)
+        $('#bookmarkList').append('<button>Click to view detail</button><button>Remove</button>')
+        $('#bookmarkList').append('</ul>')
+        
+      })
+   
+  })
 }
 
-let clearBookmarkList = () => 
-{
-    $("#bookmarkList ul").html("");
+let addBookmark = function (bookmark) {
+  //console.log(JSON.stringify(bookmark));
+
+  fetch(urlEndpoint, {
+    method: 'post', body: JSON.stringify(bookmark), headers: { 'Content-Type': 'application/json' },
+  })
+    
+  .then(res => res.json())
+  .then(json => {
+    console.log(json)
+  loadBookMarks();
+  });
+
+
+
+
+
+  //bookmarks.push(bookmark);
+  //return bookmarks
+
 }
+
+let clearBookmarkList = function () {
+  $('#bookmarkList').html('');
+}
+
+let deleteButton = function () {
+  
+}
+
+
+$(main);
+
+
--- store.js
+++ store.js
@@ -1,16 +1,24 @@
-export const STORE =
-[{
-    title: "Google Bookmark",
-    url: "www.google.com",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi auctor felis ac ante tincidunt, nec mattis ante vestibulum. Morbi et arcu eu odio tincidunt feugiat nec sit amet libero. Proin justo nibh, dignissim eu tincidunt porttitor, placerat nec magna. Sed dui leo, sodales a pellentesque a, fermentum tempor mauris. Etiam ac posuere velit, ac euismod risus. Sed libero ante, eleifend laoreet enim eu, consequat egestas elit. Aliquam eget lacus eget ante sollicitudin bibendum sit amet vitae sapien. Etiam congue interdum blandit. Vestibulum ut nulla nisl. Nulla tristique at libero et rutrum. Aliquam in tortor in quam cursus aliquam. Donec ultrices lectus ac molestie dictum. Ut sodales metus in risus varius fermentum."
-},
-{
-    title: "Yahoo Bookmark",
-    url: "www.yahoo.com",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi auctor felis ac ante tincidunt, nec mattis ante vestibulum. Morbi et arcu eu odio tincidunt feugiat nec sit amet libero. Proin justo nibh, dignissim eu tincidunt porttitor, placerat nec magna. Sed dui leo, sodales a pellentesque a, fermentum tempor mauris. Etiam ac posuere velit, ac euismod risus. Sed libero ante, eleifend laoreet enim eu, consequat egestas elit. Aliquam eget lacus eget ante sollicitudin bibendum sit amet vitae sapien. Etiam congue interdum blandit. Vestibulum ut nulla nisl. Nulla tristique at libero et rutrum. Aliquam in tortor in quam cursus aliquam. Donec ultrices lectus ac molestie dictum. Ut sodales metus in risus varius fermentum."
-},
-{
-    title: "AOL Bookmark",
-    url: "www.aol.com",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi auctor felis ac ante tincidunt, nec mattis ante vestibulum. Morbi et arcu eu odio tincidunt feugiat nec sit amet libero. Proin justo nibh, dignissim eu tincidunt porttitor, placerat nec magna. Sed dui leo, sodales a pellentesque a, fermentum tempor mauris. Etiam ac posuere velit, ac euismod risus. Sed libero ante, eleifend laoreet enim eu, consequat egestas elit. Aliquam eget lacus eget ante sollicitudin bibendum sit amet vitae sapien. Etiam congue interdum blandit. Vestibulum ut nulla nisl. Nulla tristique at libero et rutrum. Aliquam in tortor in quam cursus aliquam. Donec ultrices lectus ac molestie dictum. Ut sodales metus in risus varius fermentum."
-}]
+
+
+
+const bookmarks =
+    [
+        {
+            id: 'x56w',
+            title: 'Title 1',
+            rating: 3,
+            url: 'http://www.title1.com',
+            description: 'lorem ipsum dolor sit',
+            expanded: false
+        },
+        {
+            id: '6ffw',
+            title: 'Title 2',
+            rating: 5,
+            url: 'http://www.title2.com',
+            description: 'dolorum tempore deserunt',
+            expanded: false
+        }
+
+    ]
+    export default bookmarks;
