538bb7179fc1c4c1acaecff862d67defe0f5a828
Andy Charles <66924525+developedbyandy@users.noreply.github.com>
2020-09-03 02:25:11 -0400

    Final BookMark Project (Everything Works)

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,32 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-
-const path = require("path");
-
-module.exports = { entry: './src/index.js', output: { 
-    filename: 'main.js', path: path.resolve(__dirname, 'public'), 
-    library: 'mainLib', } 
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'index_bundle.js'
+  },
+  mode: 'development',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  }
 };
